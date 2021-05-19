6a662a3c6c6177ef6e86139061662e903c6ac2a2
brick0123 <tlfanfvk@naver.com>
2021-05-19 22:29:36 +0900

    JSP 추가

---
+++ src/test/java/com/jinho/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.jinho.servlet.domain.member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class MemberRepositoryTest {
+
+  MemberRepository memberRepository = MemberRepository.getInstance();
+
+  @AfterEach
+  void tearDown() {
+    memberRepository.clearStore();
+  }
+
+  @Test
+  void save() {
+    // given
+    Member member = new Member("hello", 20);
+
+    // when
+    Member saveMember = memberRepository.save(member);
+
+    // then
+    Member findMember = memberRepository.findById(saveMember.getId());
+    assertThat(findMember).isEqualTo(saveMember);
+  }
+
+  @Test
+  void findAll() {
+    // given
+    Member member1 = new Member("hello", 20);
+    Member member2 = new Member("hello", 30);
+
+    memberRepository.save(member1);
+    memberRepository.save(member2);
+
+    // when
+    List<Member> result = memberRepository.findAll();
+
+    // then
+    assertThat(result.size()).isEqualTo(2);
+    assertThat(result).contains(member1, member2);
+
+  }
+
+}
