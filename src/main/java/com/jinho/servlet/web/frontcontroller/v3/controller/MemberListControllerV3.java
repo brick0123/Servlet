7c3230297b5341ae23e69470faeae890104fa42d
brick0123 <tlfanfvk@naver.com>
2021-05-27 23:53:37 +0900

    FrontController Adapter 패턴 이용

---
+++ src/main/java/com/jinho/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -0,0 +1,22 @@
+package com.jinho.servlet.web.frontcontroller.v3.controller;
+
+import com.jinho.servlet.domain.member.Member;
+import com.jinho.servlet.domain.member.MemberRepository;
+import com.jinho.servlet.web.frontcontroller.ModelView;
+import com.jinho.servlet.web.frontcontroller.v3.ControllerV3;
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV3 implements ControllerV3 {
+
+  private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+  @Override
+  public ModelView process(Map<String, String> paramMap) {
+    List<Member> members = memberRepository.findAll();
+
+    ModelView mv = new ModelView("members");
+    mv.getModel().put("members", members);
+    return mv;
+  }
+}
---
+++ src/main/java/com/jinho/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,15 @@
+package com.jinho.servlet.web.frontcontroller.v5;
+
+import com.jinho.servlet.web.frontcontroller.ModelView;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface MyHandlerAdapter {
+
+  boolean supports(Object handler);
+
+  ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+
+}
