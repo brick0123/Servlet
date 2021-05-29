0283db1b36046d53b5cfb69122d3245d4b42699b
brick0123 <tlfanfvk@naver.com>
2021-05-29 22:55:02 +0900

    spring-mvc를 이용한 Controller

---
+++ src/main/java/com/jinho/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,17 @@
+package com.jinho.servlet.web.springmvc.old;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+@Component("/spring-mvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+
+  @Override
+  public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    System.out.println("MyHttpRequestHandler.handleRequest");
+  }
+}
